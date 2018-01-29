8010dbd5f7690b9cc1466600954f3bd420e7a5bb
Bjørn Erik Rønningen <be.ronningen@gmail.com>
2018-01-29 12:27:52 +0100

    Initial commit

---
+++ src/main/java/no/ber/champchamps/ChampchampsApplication.java
@@ -0,0 +1,12 @@
+package no.ber.champchamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampchampsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampchampsApplication.class, args);
+	}
+}
